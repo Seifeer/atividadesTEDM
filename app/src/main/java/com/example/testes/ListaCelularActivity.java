c388b1ff8a3e19cb13b6044ede394f1b9accfa6c
Ermeson16 <ermeson16000@gmail.com>
2019-05-20 15:36:53 -0300

    listagens

---
+++ app/src/main/java/com/example/testes/ListaCelularActivity.java
@@ -0,0 +1,13 @@
+package com.example.testes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaCelularActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_celular);
+    }
+}
