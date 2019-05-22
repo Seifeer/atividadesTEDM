1159984e7cecb742e908a96ab7d0e142b108a880
Ermeson16 <ermeson16000@gmail.com>
2019-05-22 17:06:16 -0300

    RecyclerView

--- app/src/main/java/com/example/testes/Celular.java
+++ app/src/main/java/com/example/testes/Celular.java
@@ -1,5 +1,55 @@
 package com.example.testes;
 
-public class Celular {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Celular {
+    private String Marca;
+    private String Modelo;
+    private double Valor;
+
+    public Celular(String marca, String modelo, int valor){
+        this.Marca=marca;
+        this.Modelo=modelo;
+        this.Valor=valor;
+    }
+
+    public static List<Celular> getCelulares(){
+        List<Celular> celulares= new ArrayList<>();
+
+        celulares.add(new Celular("Apple", "IPhone XR", 9000));
+        celulares.add(new Celular("Apple", "IPhone 8", 3000));
+        celulares.add(new Celular("Samsung", "A5", 300));
+        celulares.add(new Celular("Apple", "IPhone 5C", 900));
+        celulares.add(new Celular("Samsung", "J4", 600));
+        celulares.add(new Celular("Samsung", "S10", 6000));
+        celulares.add(new Celular("Apple", "IPhone 4S", 400));
+        celulares.add(new Celular("Samsung", "A7", 1000));
+
+        return  celulares;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public String getModelo() {
+        return Modelo;
+    }
+
+    public double getValor() {
+        return Valor;
+    }
+
+    public void setMarca(String marca) {
+        Marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        Modelo = modelo;
+    }
+
+    public void setValor(double valor) {
+        Valor = valor;
+    }
 }
---
+++ app/src/main/java/com/example/testes/CelularAdapter.java
@@ -0,0 +1,14 @@
+package com.example.testes;
+
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+public class CelularAdapter {
+    extends RecyclerView.Adapter<CelularAdapter.CelularViewHolder>{
+
+        private List<Celular> celulars;
+
+    }
+}
+
